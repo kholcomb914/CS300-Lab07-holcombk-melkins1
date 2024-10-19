a55bb15082952dd3df7719863f91daa8d4b6c064
kholcomb914 <k1t.holcomb@outlook.com>
2024-10-18 21:35:42 -0500

    Milestone 1 first attempt
    -Kaustubh Holcomb

--- Labyrinth.cpp
+++ Labyrinth.cpp
@@ -1,8 +1,27 @@
 #include "Labyrinth.h"
 
-bool isPathToFreedom(MazeCell* start, const std::string& moves) {
-    /* TODO: Delete this comment and the remaining lines, then implement this function. */
-    (void) start;
-    (void) moves;
-    return false;
+bool isPathToFreedom(MazeCell* start, const std::string& moves) 
+{
+    bool foundSpellbook = false;
+    bool foundPotion = false;
+    bool foundWand = false;
+    MazeCell* currentCell = start;
+    
+    if(currentCell->whatsHere == Item::SPELLBOOK) foundSpellbook = true;
+    else if(currentCell->whatsHere == Item::POTION) foundPotion = true;
+    else if(currentCell->whatsHere == Item::WAND) foundWand = true;
+    
+    for(int i = 0; i < moves.size(); i++)
+    {
+        if(moves[i] == 'N') currentCell = currentCell->north;
+        else if(moves[i] == 'S') currentCell = currentCell->south;
+        else if(moves[i] == 'E') currentCell = currentCell->east;
+        else currentCell = currentCell->west;
+        
+        if(currentCell->whatsHere == Item::SPELLBOOK) foundSpellbook = true;
+        else if(currentCell->whatsHere == Item::POTION) foundPotion = true;
+        else if(currentCell->whatsHere == Item::WAND) foundWand = true;
+    }
+    
+    return foundWand && foundPotion && foundSpellbook;
 }
